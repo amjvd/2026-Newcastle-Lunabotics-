98b731b1986879ec162f082bdf47ab83469e94a4
Amjad Hamed <122484530+amjvd@users.noreply.github.com>
2026-06-15 00:52:08 +0100

    Change video stream format from RTP to MPEG-TS

--- start_cameras.sh
+++ start_cameras.sh
@@ -13,14 +13,13 @@ echo "=========================================================="
 
 # 1. Front Camera (Port 5010)
 echo "Starting Front Camera on /dev/video0 -> Port 5010..."
-gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw,width=640,height=480,framerate=15/1 ! videoconvert ! x264enc tune=zerolatency bitrate=1200 speed-preset=ultrafast ! h264parse ! rtph264pay ! udpsink host=$LAPTOP_IP port=5010 sync=false &
-
+gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw,width=640,height=480,framerate=15/1 ! videoconvert ! x264enc tune=zerolatency bitrate=1200 speed-preset=ultrafast ! h264parse ! mpegtsmux ! udpsink host=$LAPTOP_IP port=5010 sync=false &
 # 2. Rear Camera (Port 5011)
 echo "Starting Rear Camera on /dev/video4 -> Port 5011..."
-gst-launch-1.0 v4l2src device=/dev/video4 ! video/x-raw,width=640,height=480,framerate=15/1 ! videoconvert ! x264enc tune=zerolatency bitrate=1200 speed-preset=ultrafast ! h264parse ! rtph264pay ! udpsink host=$LAPTOP_IP port=5011 sync=false &
+gst-launch-1.0 v4l2src device=/dev/video4 ! video/x-raw,width=640,height=480,framerate=15/1 ! videoconvert ! x264enc tune=zerolatency bitrate=1200 speed-preset=ultrafast ! h264parse ! mpegtsmux ! udpsink host=$LAPTOP_IP port=5011 sync=false &
 
 # 3. Scraper Camera (The dual-lens one) (Port 5012)
 echo "Starting Scraper Camera on /dev/video8 -> Port 5012..."
-gst-launch-1.0 v4l2src device=/dev/video8 ! video/x-raw,width=640,height=480,framerate=15/1 ! videoconvert ! x264enc tune=zerolatency bitrate=1200 speed-preset=ultrafast ! h264parse ! rtph264pay ! udpsink host=$LAPTOP_IP port=5012 sync=false &
+gst-launch-1.0 v4l2src device=/dev/video8 ! video/x-raw,width=640,height=480,framerate=15/1 ! videoconvert ! x264enc tune=zerolatency bitrate=1200 speed-preset=ultrafast ! h264parse ! mpegtsmux ! udpsink host=$LAPTOP_IP port=5012 sync=false &
 
 wait
